a02b7fa2df3c5336eda0da225d484404d07f6ca1
iamnotcoding <shw42909@gmail.com>
2025-05-27 17:06:28 +0900

    fix code quality issues

--- parse.py
+++ parse.py
@@ -1,3 +1,9 @@
+'''
+A module to parse an .osu file (osu maina) and store its data in a structured format.
+'''
+
+# pylint: disable=too-few-public-methods
+
 class MainaMap:
     """notes should be sotred by time"""
 
@@ -8,17 +14,17 @@ class MainaMap:
 
 
 def get_key_count(path: str) -> int:
-    file = open(path, "r", encoding="utf-8")
+    '''
+    Returns the maina key count from an osu! map file.
+    '''
+    with open(path, "r", encoding="utf-8") as file:
+        lines = file.readlines()
 
-    lines = file.readlines()
+        for line in lines:
+            if line.startswith("CircleSize"):
+                return int(line.split(":")[1].strip())
 
-    for line in lines:
-        if line.startswith("CircleSize"):
-            return int(line.split(":")[1].strip())
-
-    file.close()
-
-    raise ValueError("Key count not found in the map file.")
+        raise ValueError("Key count not found in the map file.")
 
 
 def parse_map(path: str) -> MainaMap:
@@ -30,35 +36,34 @@ def parse_map(path: str) -> MainaMap:
     data: dict[float, list[int]] = {}
     key_count = get_key_count(path)
 
-    file = open(path, "r", encoding="utf-8")
+    with open(path, "r", encoding="utf-8") as file:
+        lines = file.readlines()
 
-    lines = file.readlines()
+        object_start_line = 0
 
-    object_start_line = 0
+        for line in lines:
+            object_start_line += 1
 
-    for line in lines:
-        object_start_line += 1
+            if line.strip() == "[HitObjects]":
+                break
 
-        if line.strip() == "[HitObjects]":
-            break
+        for line in lines[object_start_line:]:
+            tokens = line.split(",")
+            index = int(int(tokens[0]) * key_count / 512 + tolerance)
+            time = int(tokens[2])
 
-    for line in lines[object_start_line:]:
-        tokens = line.split(",")
-        index = int(int(tokens[0]) * key_count / 512 + tolerance)
-        time = int(tokens[2])
+            if time not in data:
+                data[time] = []
 
-        if time not in data:
-            data[time] = []
+            data[time].append(index)
 
-        data[time].append(index)
-
-    note_count = len(lines[object_start_line:])
+        note_count = len(lines[object_start_line:])
 
     return MainaMap(key_count, dict(sorted(data.items())), note_count)
 
 
 if __name__ == "__main__":
-    map = parse_map("example.osu")
-    print(f"Key Count: {map.key_count}")
-    print(f"data: {map.data}")
-    print(f"Note Count: {map.note_count}")
+    ex_m = parse_map("example.osu")
+    print(f"Key Count: {ex_m.key_count}")
+    print(f"data: {ex_m.data}")
+    print(f"Note Count: {ex_m.note_count}")
