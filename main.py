672a1ff190e04182cabe8588091f95556aeb3662
iamnotcoding <shw42909@gmail.com>
2025-05-27 17:20:53 +0900

    add a separate functino for printing the output

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-This moudule gets a file name from the command line, 
+This moudule gets a file name from the command line,
 and prints the pattern stats of a osu maina map in a JSON format.
 """
 
@@ -13,9 +13,9 @@ import calc
 def butify_pattern_stats(
     pattern_stats: dict[calc.PatternType, float],
 ) -> dict[str, float]:
-    '''
+    """
     Replaces the enum itself with its name
-    '''
+    """
     result: dict[str, float] = {}
 
     for pattern, value in pattern_stats.items():
@@ -24,14 +24,20 @@ def butify_pattern_stats(
     return result
 
 
+def print_pattern_stats(file_path: str) -> None:
+    """
+    Prints the pattern stats of a osu maina map in a JSON format.
+    """
+    map_data = parse.parse_map(file_path)
+    pattern_stats = calc.calc_4k_pattern_stats(map_data)
+    butified_stats = butify_pattern_stats(pattern_stats)
+
+    print(json.dumps(butified_stats, indent=4, ensure_ascii=False))
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python main.py <path_to_osu_file>")
         sys.exit(1)
 
-    osu_file_path = sys.argv[1]
-    map_data = parse.parse_map(osu_file_path)
-    pattern_stats = calc.calc_4k_pattern_stats(map_data)
-    butified_stats = butify_pattern_stats(pattern_stats)
-
-    print(json.dumps(butified_stats, indent=4, ensure_ascii=False))
+    print_pattern_stats(sys.argv[1])
