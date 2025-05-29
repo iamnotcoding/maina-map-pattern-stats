86a0d0c83eca08fb4757fe7ae7120cfd211a7eb7
iamnotcoding <shw42909@gmail.com>
2025-05-29 16:04:50 +0900

    temp

--- calc.py
+++ calc.py
@@ -5,6 +5,7 @@ This module contains classes and functions to calculate pattern statistics
 
 import json
 from enum import Enum
+import math
 
 from parse import MainaMap, parse_map
 
@@ -195,7 +196,7 @@ def get_hold_note_lowest_time_diff(hold_notes: dict[float, list[int]]) -> float:
 
     return result
 
-def get_closest_release_time(hold_time : float, hold_note_index : int, release_notes : dict[float,list[int]]])->float|None:
+def get_closest_release_time(hold_time : float, hold_note_index : int, release_notes : dict[float,list[int]])->float|None:
     for release_time, note_objs in release_notes.items():
         if release_time > hold_time and hold_note_index in note_objs:
             return release_time
@@ -203,7 +204,7 @@ def get_closest_release_time(hold_time : float, hold_note_index : int, release_n
     return None
 
 # TODO : add support for higher key modes than 4k in a separate function
-def calc_4k_hold_note_pattern_stats(m: MainaMap) -> dict[PatternType, float]:
+def calc_4k_hold_note_pattern_stats(hold_notes : dict[float, list[int]]) -> dict[PatternType, float]:
     """
     Calculates pattern stats for a 4k map.
     """
@@ -220,14 +221,14 @@ def calc_4k_hold_note_pattern_stats(m: MainaMap) -> dict[PatternType, float]:
         PatternType.SPLIT_TRILL: 6,
     }
 
-    prev_notes = list(m.hold_notes.values())[0]
-    prev_time = list(m.hold_notes.keys())[0]
+    prev_notes = list(hold_notes.values())[0]
+    prev_time = list(hold_notes.keys())[0]
 
-    prev_prev_notes = list(m.hold_notes.values())[1]
+    prev_prev_notes = list(hold_notes.values())[1]
 
-    line_count = len(m.hold_notes)
+    line_count = len(hold_notes)
 
-    for time, notes in list(m.hold_notes.items())[2:]:
+    for time, notes in list(hold_notes.items())[2:]:
         pattern_type = get_pattern_type(prev_prev_notes, prev_notes, notes)
         pattern_stats[pattern_type] += get_point_from_hold_note_time_diff(
             time - prev_time
@@ -239,8 +240,90 @@ def calc_4k_hold_note_pattern_stats(m: MainaMap) -> dict[PatternType, float]:
         prev_time = time
 
     # This is gonnna be inaccurate but it's better than nothing
-    time, notes = list(m.hold_notes.items())[-1]
-    prev_time = list(m.hold_notes.keys())[-2]
+    time, notes = list(hold_notes.items())[-1]
+    prev_time = list(hold_notes.keys())[-2]
+
+    pattern_type = get_pattern_type(prev_notes, notes, [3])  # Dummy note
+    pattern_stats[pattern_type] += get_point_from_hold_note_time_diff(time - prev_time)
+
+    for pattern in pattern_stats:
+        if pattern == PatternType.OVERALL:
+            continue
+
+        pattern_stats[pattern] *= pattern_weights[pattern]
+        pattern_stats[pattern] /= line_count
+
+    pattern_stats[PatternType.OVERALL] = sum(pattern_stats.values())
+
+    return pattern_stats
+
+def logistic(x : float, midpointOffset : float, multiplier : float, maxValue : float = 1) -> float:
+    return maxValue / (1 + math.exp(multiplier * (midpointOffset - x)))
+
+def StrainValueOf(current : int)->float:
+            maniaCurrent = current;
+            startTime :float= maniaCurrent.StartTime;
+            endTime :float= maniaCurrent.EndTime;
+            column :int= maniaCurrent.BaseObject.Column;
+            isOverlapping :bool= False;
+
+            closestEndTime = Math.Abs(endTime - startTime); // Lowest value we can assume with the current information
+            holdFactor = 1.0; # Factor to all additional strains in case something else is held
+            holdAddition = 0.0; # Addition to the current note in case it's a hold and has to be released awkwardly
+
+            for (int i = 0; i < endTimes.Length; ++i)
+            {
+                # The current note is overlapped if a previous note or end is overlapping the current note body
+                isOverlapping |= Precision.DefinitelyBigger(endTimes[i], startTime, 1) &&
+                                 Precision.DefinitelyBigger(endTime, endTimes[i], 1) &&
+                                 Precision.DefinitelyBigger(startTime, startTimes[i], 1);
+
+                // We give a slight bonus to everything if something is held meanwhile
+                if (Precision.DefinitelyBigger(endTimes[i], endTime, 1) &&
+                    Precision.DefinitelyBigger(startTime, startTimes[i], 1))
+                    holdFactor = 1.25;
+
+                closestEndTime = Math.Min(closestEndTime, Math.Abs(endTime - endTimes[i]));
+            }
+# TODO : add support for higher key modes than 4k in a separate function
+def calc_4k_release_note_pattern_stats(hold_notes : dict[float,list[int]], release_notes : dict[float, list[int]]) -> dict[PatternType, float]:
+    """
+    Basically uses the same algorithm that osu!lazor does
+    """
+    pattern_stats = {pattern: 0.0 for pattern in PatternType}
+    pattern_weights: dict[PatternType, float] = {
+        PatternType.SINGLE_STREAM: 0.91,
+        PatternType.JUMP_STREAM: 1.92,
+        # lower then split trill since it may not be a trill. 
+        PatternType.HAND_STREAM: 4.2, 
+        PatternType.SPEED_JACK: 2.2,
+        PatternType.LIGHT_CHORD_JACK: 2.8,
+        PatternType.DENSE_CHORD_JACK: 5.3,
+        PatternType.JUMP_TRILL: 2.7,
+        PatternType.SPLIT_TRILL: 6,
+    }
+
+    prev_notes = list(release_notes.values())[0]
+    prev_time = list(release_notes.keys())[0]
+
+    prev_prev_notes = list(release_notes.values())[1]
+
+    line_count = len(release_notes)
+
+    for time, notes in list(release_notes.items())[2:]:
+        pattern_type = get_pattern_type(prev_prev_notes, prev_notes, notes)
+        pattern_stats[pattern_type] += get_point_from_hold_note_time_diff(
+            time - prev_time
+        )
+
+        prev_prev_notes = prev_notes
+
+        prev_notes = notes
+        prev_time = time
+
+    # This is gonnna be inaccurate but it's better than nothing
+    time, notes = list(release_notes.items())[-1]
+    prev_time = list(release_notes.keys())[-2]
 
     pattern_type = get_pattern_type(prev_notes, notes, [3])  # Dummy note
     pattern_stats[pattern_type] += get_point_from_hold_note_time_diff(time - prev_time)
@@ -276,7 +359,7 @@ def from_file(file_path: str) -> dict[str, float]:
     Reads a osu! map file and returns the pattern stats in a butified format.
     """
     m = parse_map(file_path)
-    pattern_stats = calc_4k_hold_note_pattern_stats(m)
+    pattern_stats = calc_4k_hold_note_pattern_stats(m.hold_notes)
     butified_stats = butify_pattern_stats(pattern_stats)
     return butified_stats
 
